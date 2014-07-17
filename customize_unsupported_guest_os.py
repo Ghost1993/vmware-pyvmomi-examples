eba7a95363185e33dda1bf41bdbcd092e4019117
Reubenur Rahman <reuben.13@gmail.com>
2014-07-17 12:58:15 +0530

    Added pid check for execution failures inside VM

--- customize_unsupported_guest_os.py
+++ customize_unsupported_guest_os.py
@@ -5,13 +5,12 @@ All Rights Reserved
 '''
 
 import atexit
-import argparse
 import sys
 import time
 
 from pyVmomi import vim, vmodl
 from pyVim import connect
-from pyVim.connect import Disconnect, SmartConnect
+from pyVim.connect import Disconnect
 
 inputs = {'vcenter_ip': '15.22.10.11',
           'vcenter_password': 'Password123',
@@ -46,14 +45,30 @@ def exec_command(content, vm, creds, args, program_path):
     return output
 
 
+def verify_process(content, vm, creds, pid, message):
+    pids = []
+    processes = content.guestOperationsManager.processManager.\
+    ListProcessesInGuest(vm=vm, auth=creds)
+    for process in processes:
+        pids.append(process.pid)
+    if pid in pids:
+        if message == 'output':
+            print "Successfully created script file to customize"
+    else:
+        if message == 'output':
+            print "Something went wring while creating the script file. Couldn't configure VM"
+        elif message == 'permission':
+            print "Something went wring while giving permission to the script file. Couldn't configure VM"
+        elif message == 'execute':
+            print "Something went wring while executing the script file. Couldn't configure VM"
+
+
 def main():
     try:
         si = None
         try:
             print "Trying to connect to VCENTER SERVER . . ."
-            # Vsphere 5.1 = "vim.version.version8"
-            #Vsphere 5.5 = "vim.version.version9"
-            si = connect.Connect(inputs['vcenter_ip'], 443, inputs['vcenter_user'], inputs['vcenter_password'], version="vim.version.version8")
+            si = connect.SmartConnect('https', inputs['vcenter_ip'], 443, inputs['vcenter_user'], inputs['vcenter_password'])
         except IOError, e:
             pass
             atexit.register(Disconnect, si)
@@ -63,7 +78,7 @@ def main():
         content = si.RetrieveContent()
 
         vm = get_obj(content, [vim.VirtualMachine], inputs['vm_name'])
-
+        print vm.name
         if vm.runtime.powerState != 'poweredOn':
             print "WARNING:: Power on your VM before customizing"
             sys.exit()
@@ -93,16 +108,14 @@ fi\n\
 ' >> /home/customize_script.sh""" % (inputs['vm_ip'], inputs['netmask'], inputs['gateway'], inputs['hostname'])
 
         output = exec_command(content, vm, creds, args, '/bin/echo')
-
         print "Waiting for the Customization script to generate..."
+        verify_process(content, vm, creds, output, 'output')
         time.sleep(10)
         permission = exec_command(content, vm, creds, "+x '/home/customize_script.sh'", '/bin/chmod')
-        if not output and permission or output and permission < 0:
-            print "ERROR:: Something went wrong while creating customization script. Manually verify"
-        else:
-            print "Successfully created the Customization script"
+        verify_process(content, vm, creds, permission, 'permission')
+        execute = exec_command(content, vm, creds, "/home/customize_script.sh", '/usr/bin/sudo')
+        verify_process(content, vm, creds, execute, 'execute')
 
-        exec_command(content, vm, creds, "/home/customize_script.sh", '/usr/bin/sudo')
         print "Successfully customized VM", inputs['vm_name']
 
         print "Restarting VM to apply hostname change"
